b0acb5f5c1ca9cc9ebbce6d6f95543c99c64354b
Salka Moses Debbarma <55789209+dsalkamoses@users.noreply.github.com>
2023-03-14 08:04:21 +0000

    server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+// ESM
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
