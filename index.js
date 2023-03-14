6ee9a66003ec895504b5c134985c2fb74c144dcf
Salka Moses Debbarma <55789209+dsalkamoses@users.noreply.github.com>
2023-03-14 08:06:54 +0000

    update

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// ESM
-import Fastify from 'fastify'
-const fastify = Fastify({
-  logger: true
-})
 const fastify = require('fastify')({
   logger: true
 })
 
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
 })
 
-fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
-})
+}
+start()
