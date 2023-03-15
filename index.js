28335fc2054b99cade9f5b95ec61b92a0a0abbff
Salka Moses Debbarma <55789209+dsalkamoses@users.noreply.github.com>
2023-03-15 14:18:24 +0000

    update port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fastify.get('/', async (request, reply) => {
 //server for ecs
 const start = async () => {
   try {
-    await fastify.listen({ host: '0.0.0.0', port: 8080 })
+    await fastify.listen({ host: '0.0.0.0', port: 80 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
